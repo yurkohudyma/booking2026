eff4b13d536ccd62274610fe6e859f9e4b27ddb7
yurkohudyma <hudyma@gmail.com>
2026-03-05 06:51:23 +0200

    init commit

---
+++ src/main/java/ua/hudyma/Booking2026SpringApp.java
@@ -0,0 +1,11 @@
+package ua.hudyma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booking2026SpringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Booking2026SpringApp.class);
+    }
+}
